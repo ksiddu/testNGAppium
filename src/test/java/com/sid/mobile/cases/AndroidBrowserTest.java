9c261d5ecabb9fcb5f08e44c02894eb26c76158c
Gavisiddappa Kampli <siddu.kampli@shopee.com>
2019-03-17 15:44:54 +0800

    fixed the scripts, added wait

--- src/test/java/com/sid/mobile/cases/AndroidBrowserTest.java
+++ src/test/java/com/sid/mobile/cases/AndroidBrowserTest.java
@@ -10,55 +10,63 @@ import io.appium.java_client.AppiumDriver;
 import org.testng.annotations.BeforeMethod;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class AndroidBrowserTest {
 	AppiumDriver<WebElement> driver;
 	PropertyUtils prop = new PropertyUtils();
+	WebDriverWait wait;
 
 	@BeforeMethod
 	public void beforeMethod() throws MalformedURLException {
 
 		driver = DriverProvider.getAndroidBrowserDriver();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 20);
 	}
 
 	@Test
-	public void testLoginWithValidCredentials() {
+	public void testLoginWithValidCredentials() throws InterruptedException {
+
 		String loginUrl = prop.getValue("login_url");
 		driver.get(loginUrl);
 
-		System.out.println("MAIN PAGE TITLE: " + driver.getTitle());
-		Assert.assertEquals(driver.getTitle(), "My Store");
-		// My Store
-		System.out.println("LOGIN PAGE TITLE: " + driver.getTitle());
-		Assert.assertEquals(driver.getTitle(), "Login - My Store");
-		// Login - My Store
-
 		WebElement signInLink = driver.findElement(By.linkText("Sign in"));
-		WebElement userName = driver.findElement(By.id("email"));
-		WebElement password = driver.findElement(By.id("passwd"));
-		WebElement button = driver.findElement(By.className("SubmitLogin"));
+
+		System.out.println("HOME PAGE TITLE: " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), prop.getValue("homepage_title"));
 
 		signInLink.click();
+		System.out.println("LOGIN PAGE TITLE: " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), prop.getValue("loginpage_title"));
+
+		WebElement userName = driver.findElement(By.id("email"));
+		WebElement password = driver.findElement(By.id("passwd"));
+		WebElement button = driver.findElement(By.id("SubmitLogin"));
+
 		userName.sendKeys(prop.getValue("username"));
 		password.sendKeys(prop.getValue("password"));
+
+		wait.until(ExpectedConditions.elementToBeClickable(button));
 		button.click();
+
 		System.out.println("Login details entered - testLoginWithValidCredentials()");
 		System.out.println("============================================");
-		// My account - My Store
 
-		String expectedText = prop.getValue("homepage_title");
-		String actualText = driver.getTitle();
-		Assert.assertEquals(actualText, expectedText);
+		System.out.println("DASHBOARD PAGE TITLE: " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), prop.getValue("dashboardpage_title"));
 
 		WebElement signOutBtn = driver.findElement(By.cssSelector(".logout"));
 		signOutBtn.click();
 
-		Assert.assertEquals(driver.getTitle(), "Login - My Store");
+		Assert.assertEquals(driver.getTitle(), prop.getValue("loginpage_title"));
 
 	}
 
